24b34e39cd8c5cce87da0a4e7eb16c5bcc5b9fea
Lauren <31517766+unikornintech@users.noreply.github.com>
2019-08-02 18:48:59 -0400

    Create keys_format.py
    add to hide api keys

---
+++ keys_format.py
@@ -0,0 +1,8 @@
+# keys_format shares the same format as keys.py.
+# This file is not meant to be used in the main script.
+# Instead, copy this file into keys.py and put your own
+# keys there.
+CONSUMER_KEY = 'AAAAAAA'
+CONSUMER_SECRET = 'BBBBBB'
+ACCESS_KEY = 'CCCCCC'
+ACCESS_SECRET = 'DDDDDD'
